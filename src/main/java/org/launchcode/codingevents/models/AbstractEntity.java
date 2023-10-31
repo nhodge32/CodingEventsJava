fd80c077fce27eaaf7d62ef0e2c8d751a1140ec2
Natasha <nhodg2@gmail.com>
2023-10-30 19:03:28 -0500

    Add Abstract Entity class

--- src/main/java/org/launchcode/codingevents/models/AbstractEntity.java
+++ src/main/java/org/launchcode/codingevents/models/AbstractEntity.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevents.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 
